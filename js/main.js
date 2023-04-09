6fb276ffa8aaed4e3fde2152fa597a7f1a7cdc4b
Devveking08 <d_pandya164813@fanshaweonline.ca>
2023-04-09 16:35:00 -0700

    js
    js here

--- js/main.js
+++ js/main.js
@@ -10,3 +10,17 @@ function toggleMenu() {
 }
 
 menu.addEventListener("click", toggleMenu);
+
+function toggleDiv(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+$('.toggle-btn').on('click', function() {
+  $(this).toggleClass('active')
+});
